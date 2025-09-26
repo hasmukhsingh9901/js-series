ad6a16369140a0d36637421e1e4c13e5259696b0
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-26 15:31:37 +0000

    conversion

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,17 @@
+// let score = "99"
+// let score = "99and"
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+
+// let value_number = Number(score)
+// console.log(typeof value_number);
+// console.log(value_number)
+
+
+let isLoogedIn =0;
+
+let bool = String(isLoogedIn)
+
+console.log(typeof(bool))
