25309bafbbda9c52a3ba3fc166be5acc017d1470
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-26 14:59:01 +0000

    vaiables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const account_id = 34892385920891823;
+let account_email = "hasmukh.singh.meta@gmail.com"
+var account_password = "123456"
+account_place = "LA";
+// account_id = 43454656564564356456 // not allowed
+
+
+account_email = "hasmukh.singh.google@gmail.com";
+account_password = "qwygeuih"
+account_place = "California"
+
+// Scopes
+
+console.table([account_email, account_password, account_place])
+
+
