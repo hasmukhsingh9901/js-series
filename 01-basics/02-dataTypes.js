ebef21f04a7ce3e47cd835d3061d4846c4c6079d
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-26 15:15:22 +0000

    Data types

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// treat all JS code as newer version
+// alert("Hello") // we are nodejs not browser
+
+
+let name = "uhi";
+let age =444443;
+let isLoggedIn = true;
+
+
+
+// number  = 2 power 53
+// bigInt 
+// String
+// boolean
+// null
+// undefined 
+// symbol => unique 
+
+
+
+// Object -> 
+// console.log(typeof);
+
+
