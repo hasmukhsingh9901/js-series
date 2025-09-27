33ed63c1fb2f1c369e51b5039119132c4d098e5f
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-27 06:14:48 +0000

    Math and num

---
+++ 01-basics/09-math.js
@@ -0,0 +1,39 @@
+const score = 5325;
+// console.log(score);
+
+const balance = new Number(100);
+
+
+// console.log(balance);
+
+const oth = 2323534.2345
+// console.log(oth.toPrecision(3));
+
+
+
+const val = 100000;
+
+console.log(val.toLocaleString(
+    'en-IN'
+));
+
+
+// Maths
+
+// console.log(Math.abs(-4)); // 
+// console.log(Math.round(4.6)); // 
+// console.log(Math.ceil(4.2)); // 
+// console.log(Math.floor(4.9)); // 
+// console.log(Math.min(4.9,77,4,3,4)); // 
+// console.log(Math.max(4,4,4,5,7,5)); // 
+
+
+
+console.log(Math.random() * 10);
+
+
+const min = 10;
+const max = 20;
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
