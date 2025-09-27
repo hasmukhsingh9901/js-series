78e5e4a952880e4fe2e468ecac59da61e0e079fb
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-27 03:45:09 +0000

    conversion

---
+++ 01-basics/04-operations.js
@@ -0,0 +1,15 @@
+// Operations
+
+
+// console.log("1" + 2);
+// console.log(2 - "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+let game_counter = 100
+
+game_counter++; // Post  -> value get after increment 
+++game_counter // Prefix -> value get before increment
+console.log(game_counter)
