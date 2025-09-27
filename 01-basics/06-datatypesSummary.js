57af04f3e296070f6cf87e62cdbb19837b8b57b5
Hasmukh Singh <94060756+hasmukhsingh9901@users.noreply.github.com>
2025-09-27 04:40:09 +0000

    revision

---
+++ 01-basics/06-datatypesSummary.js
@@ -0,0 +1,27 @@
+// Primitive and Non primitive
+
+// Primitive -> String, Bol,Number, symbol,NUll , undefined , BigInt
+
+
+// Dynamic Typed
+
+//Reference Type (Non primitive) 
+
+// Array , Object, Functions
+
+
+const fu = function(){}
+
+console.log(typeof(fu));
+
+
+
+
+
+
+
+
+const id = Symbol("134");
+const another_id = Symbol("34y5t8794");
+
+console.log(id===another_id)
